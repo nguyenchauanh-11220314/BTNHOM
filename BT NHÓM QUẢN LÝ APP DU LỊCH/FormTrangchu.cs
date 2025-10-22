0d2ff4bbde60e6c707663ad56d2a5e4de07ea717
nguyenchauanh-11220314 <chauanhnguyen0310@gmail.com>
2025-10-22 18:50:03 +0700

    feat(FormTrangchu): tạo form trang chủ + layout trống

---
+++ BT NHÓM QUẢN LÝ APP DU LỊCH/FormTrangchu.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BT_NHÓM_QUẢN_LÝ_APP_DU_LỊCH
+{
+    public partial class FormTrangchu : Form
+    {
+        public FormTrangchu()
+        {
+            InitializeComponent();
+        }
+    }
+}
